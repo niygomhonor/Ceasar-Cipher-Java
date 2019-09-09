81ae4f5851bb5444a74341913e91009f2dc2b785
Imaraika <inange2013@gmail.com>
2019-09-09 09:53:54 +0200

    Add form

--- src/test/java/CeasarCipherTest.java
+++ src/test/java/CeasarCipherTest.java
@@ -1,22 +1,32 @@
+
 import caesar.CeasarCipher;
 import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class CeasarCipherTest{
 
 
-    CeasarCipher testCipher=new CeasarCipher();
+    CeasarCipher encryptCipher=new CeasarCipher();
+
+//    @Test
+//    public void runCeasarCipher_Nothing_to_do_empty_string() {
+//
+//        assertEquals("", decryptCipher.decryption("",3));
+//        // contents of test will eventually go here.
+//    }
     @Test
     public void runCeasarCipher_Nothing_to_with_empty_string() {
 
-        assertEquals("", testCipher.encryption("",3));
+        assertEquals("", encryptCipher.caesar.encryption("",3));
         // contents of test will eventually go here.
     }
     @Test
     public void shouldNotCipherSymbols() {
-        assertEquals("", testCipher.encryption("", 3));
+        assertEquals("", encryptCipher.caesar.encryption("", 3));
 
         String symbols = "1!@#$%^&*(){}/";
-        assertEquals(symbols, testCipher.encryption(symbols, 3));
+        assertEquals(symbols, encryptCipher.caesar.encryption(symbols, 3));
     }
 
 }
